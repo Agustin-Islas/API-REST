8b4801d25032b6b39e802a97402f9bd422412a6c
Agustin-Islas <agustinislas44@hotmail.com>
2023-01-09 20:39:28 -0300

    project finalized

--- routes.js
+++ routes.js
@@ -13,4 +13,37 @@ routes.get('/', (req, res) => {
     })
 });
 
+routes.post('/', (req, res) => {
+    req.getConnection((err, conn) => {
+        if (err) return res.send(err);
+        conn.query('INSERT INTO books set ?', [req.body], (err, rows) => {
+            if (err) return res.send(err);
+
+            res.send('book added!')
+        });
+    })
+});
+
+routes.delete('/:id', (req, res) => {
+    req.getConnection((err, conn) => {
+        if (err) return res.send(err);
+        conn.query('DELETE FROM books WHERE id = ?', [req.params.id], (err, rows) => {
+            if (err) return res.send(err);
+
+            res.send('book excluded!')
+        });
+    })
+});
+
+routes.put('/:id', (req, res) => {
+    req.getConnection((err, conn) => {
+        if (err) return res.send(err);
+        conn.query('UPDATE books set ? WHERE id = ?', [req.body, req.params.id], (err, rows) => {
+            if (err) return res.send(err);
+
+            res.send('book updated!')
+        });
+    })
+});
+
 module.exports = routes;
